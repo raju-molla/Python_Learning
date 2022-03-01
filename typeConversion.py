2a39530b478179e5a2833bb344846c113e426650
raju-molla <iamraju.eu56@gmail.com>
2022-03-01 20:32:29 +0600

    py

---
+++ typeConversion.py
@@ -0,0 +1,12 @@
+# birth_year =  input("Enter your birth year: ");
+# age = 2022 - int(birth_year);
+# print(age)
+
+# calculate two number
+
+from cgi import print_form
+
+num1 = input("Enter first number ? ")
+num2 = input("Enter Second number ? ")
+ans  = float(num1) -float(num2)
+print(ans) 
---
+++ variable.py
@@ -0,0 +1,12 @@
+# a= 20
+# is_online = False
+# print(a)
+# print(is_online)
+
+# task
+
+name_patient ="john smit"
+age =20
+status=  "new"
+
+
